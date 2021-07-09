53a48e3b8d66b42096842c14aeac17c788565f45
Song Dekang <1494403731@qq.com>
2021-07-10 01:14:04 +0800

    初始DEMO仓库,添加GIT提交批处理文件

---
+++ pa.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+title GIT一键提交ALL-IN-ONE
+color 3
+chcp 65001
+
+echo;
+echo 当前目录为: %cd%
+
+if not %cd% == C:\Users\14944\Desktop\ALL-IN-ONE (
+echo 跳转目录到: ALL-IN-ONE
+cd C:\Users\14944\Desktop\ALL-IN-ONE
+echo 当前目录为: !cd!
+if !cd! == C:\Users\14944\Desktop\ALL-IN-ONE ( echo 跳转成功 ) else ( echo 跳转失败 )
+echo;
+)
+
+echo 添加变更
+git add .
+set /p message=请输入 commit 信息:
+echo 提交代码
+echo "%message%"
+git commit -m "%message%"
+echo 推送代码
+
+
+
+:: 暂停命令 输出为空
+:: pause > nul
+
+
+
+
