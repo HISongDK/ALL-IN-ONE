8b361a2d8b8b4c46f8ee96798a8f16154c21d863
songdekang <1494403731@qq.com>
2021-09-17 11:03:27 +0800

    TEST prettier jsxBracketSameLine deprecated

--- crte/src/App.jsx
+++ crte/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn&nbsp;&nbsp;&nbsp;&nbsp;React
+          Learn&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;React
         </a>
       </header>
     </div>
