821b8b58debb77dca1dcd699e740bd8da6a3ac49
宋德康 <1494403731@qq.com>
2021-11-27 22:01:26 +0800

    useContext、useReducer模拟redux

--- hook/src/index.js
+++ hook/src/index.js
@@ -2,11 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import Store from './context/storeContext'
-
-ReactDOM.render(
-    <Store.Provider>
-        <App />
-    </Store.Provider>,
-    document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
