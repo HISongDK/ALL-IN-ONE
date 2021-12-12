10d6bef1e62f361f9d614975e2c73e534b4cc2ff
宋德康 <1494403731@qq.com>
2021-12-13 00:18:30 +0800

    不成功的react-router-dom调试，以及teadesi试用

--- crte/src/index.js
+++ crte/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Routes } from 'react-router-dom'
 import App from './App'
 
+// 引入 easy-peasy 状态管理
 import { StoreProvider } from 'easy-peasy'
 import store from './store/store'
+// 引入 tea css
+import 'tea-component/dist/tea.css'
 
 ReactDOM.render(
   <StoreProvider store={store}>
-    <App />
+    <Routes>
+      <App />
+    </Routes>
   </StoreProvider>,
   document.getElementById('root'),
 )
