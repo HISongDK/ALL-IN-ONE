b1b9c67ad52f904c06538cee8c93caa790c441c4
宋德康 <1494403731@qq.com>
2021-12-15 00:01:05 +0800

    自己页面渲染自己路由可不就死循环搞崩页面了么

--- crte/src/router/config.js
+++ crte/src/router/config.js
@@ -5,7 +5,6 @@ import Icon from '../page/Icon/Icon'
 const routes = [
   { path: '/button', component: Button, title: '按钮' },
   { path: '/icon', component: Icon, title: '图标' },
-  { path: '/', component: Home, title: '主页' },
 ]
 
 export default routes
