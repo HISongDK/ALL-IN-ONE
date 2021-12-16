5ce6d5047d2783556b520d91fa8c563c362a67c0
宋德康 <1494403731@qq.com>
2021-12-16 23:42:49 +0800

    凑数

--- crte/src/router/config.js
+++ crte/src/router/config.js
@@ -1,10 +1,12 @@
 import Button from '../page/Button/Button'
 import Home from '../page/Home/Home'
 import Icon from '../page/Icon/Icon'
+import Result from '../page/Result'
 
 const routes = [
   { path: '/button', component: Button, title: '按钮' },
   { path: '/icon', component: Icon, title: '图标' },
+  { path: '/result', component: Result, title: '结果' },
 ]
 
 export default routes
