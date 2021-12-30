aaaba5e5a2c25c411ecebdff7ae820657e0553ea
宋德康 <1494403731@qq.com>
2021-12-30 23:28:32 +0800

    直接使用procss.env.NODE_ENV

--- crte/src/page/Env/Env.tsx
+++ crte/src/page/Env/Env.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function Env() {
-  return <div>{process.env.REACT_APP_ENV}</div>
+  return (
+    <>
+      <div>{process.env.REACT_APP_ENV}</div>
+      <hr />
+      <p>{process.env.NODE_ENV}</p>
+    </>
+  )
 }
 
 export default Env
