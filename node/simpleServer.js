c500682dbbfbb82c0cd7df689460d53f0cc7172c
宋德康 <1494403731@qq.com>
2022-01-06 00:05:20 +0800

    8080端口被占用，不过也没查询到

--- node/simpleServer.js
+++ node/simpleServer.js
@@ -3,6 +3,6 @@ const http = require('http')
 http.createServer(function (req, res) {
     res.write('Hello,Node.js!')
     res.end()
-}).listen(8080)
+}).listen(9527)
 
-console.log('Server started. Listening on port 8080')
+console.log('Server started. Listening on port 9527')
