53b4fb5c7017ec72a010338f30d2246d4ec4e7c4
dksong <1494403731@qq.com>
2022-03-03 12:16:19 +0800

    :memo: 手动加载微应用尝试

--- puzzle/qrtaes/src/micro/index.ts
+++ puzzle/qrtaes/src/micro/index.ts
@@ -5,12 +5,12 @@ export default [
     name: 'vue2',
     entry: '//localhost:9526',
     container: '#vue2',
-    activeRule: PATH.VUE2_PATH,
+    // activeRule: PATH.VUE2_PATH,
   },
   {
     name: 'vvtee',
     entry: '//localhost:9527',
     container: '#vue3',
-    activeRule: PATH.VUE3_PATH,
+    // activeRule: PATH.VUE3_PATH,
   },
 ]
--- puzzle/qrtaes/src/pages/Vue3/index.tsx
+++ puzzle/qrtaes/src/pages/Vue3/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { loadMicroApp } from 'qiankun'
+import microApps from '@/micro'
 
 function Vue3() {
+  useEffect(() => {
+    const microApp = loadMicroApp(microApps[1])
+    return () => {
+      microApp.unmount()
+    }
+  }, [])
   return <div id="vue3" />
 }
 
