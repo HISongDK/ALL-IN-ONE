95238ec95cfef61ea05b0f059c65ae886cd055a9
宋德康 <1494403731@qq.com>
2022-03-09 00:36:12 +0800

    :sparkles: 安装并初始 socket.io

--- node/app.js
+++ node/app.js
@@ -1,15 +1,23 @@
-const express = require('express')
+import express from 'express'
+import logger from 'morgan' // 日志器
+import chalk from 'chalk'
+import { Server } from 'socket.io'
+import http from 'http'
+import path from 'path'
+// import cors from 'cors'
+
+const __dirname = path.resolve()
+
+// 初始化 express 应用
 const app = express()
-
-const logger = require('morgan') // 日志器
 app.use(logger('dev')) // app use logger传参调用的函数之后，每次请求路由，控制台会输出相关信息
-
-// 处理 post 请求，获取请求体
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false })) // 处理 post 请求，获取请求体
 app.use(express.json())
-
-// const cors = require('cors')
 // app.use(cors()) // 解决跨域问题
+
+// 初始化 socket.io
+const httpServer = http.createServer(app)
+const io = new Server(httpServer)
 
 /**
  * 接口路由
@@ -32,4 +40,24 @@ app.post('/login', (req, res) => {
     })
 })
 
-app.listen('9000', console.log('服务正在监听 9000 端口'))
+/**
+ * Socket.IO 示例
+ */
+app.get('/socket.io', (req, res) => {
+    res.sendFile(__dirname + '/socket/index.html')
+})
+
+io.on('connect', (socket) => {
+    console.log('---  socket  ---\n', socket)
+})
+
+app.listen(
+    '9000',
+    console.log(
+        `服务正在监听 9000 端口\n打开链接: ${chalk.green(
+            'http://localhost:9000',
+        )}`,
+    ),
+)
+
+export {}
