59ed816ca7e8b628eea13bba0412b58540b7cf51
宋德康 <1494403731@qq.com>
2022-03-09 20:19:00 +0800

    :speech_balloon: 渲染伪消息列表

--- puzzle/qrtaes/src/pages/Socket/index.tsx
+++ puzzle/qrtaes/src/pages/Socket/index.tsx
@@ -5,30 +5,40 @@ import './style.scss'
 const socket = io('172.29.40.56:9000')
 
 function Socket() {
-  const [msgVal, setMsgVal] = useState('')
+  const [curMsaVal, setCurMsaVal] = useState('')
+  const [allMsgs, setAllMsgs] = useState<string[]>([])
 
   useEffect(() => {
-    socket.on('receiveMsg', (data) => console.log('---  data  ---\n', data))
+    socket.on('msg', (data) => {
+      setAllMsgs((msgs) => [...msgs, data])
+    })
 
     return () => {
       socket.disconnect()
     }
   }, [])
 
+  // 发送消息
   const onSubmit = (e: any) => {
     e.preventDefault()
-    socket.emit('sendMsg', msgVal)
-    setMsgVal('')
+    if (!curMsaVal) return false
+    socket.emit('msg', curMsaVal)
+    setAllMsgs((msgs) => [...msgs, curMsaVal])
+    setCurMsaVal('') // 发送后清空输入框，真么巧妙又理所当然的交互，竟没想到
     return false
   }
   return (
     <div className="socket">
-      <ul id="messages" />
+      <ul id="messages">
+        {allMsgs.map((msg) => (
+          <li key={Date.now() + Math.random()}>{msg}</li>
+        ))}
+      </ul>
       <form action="">
         <input
           id="m"
-          value={msgVal}
-          onChange={(e) => setMsgVal(e.target.value)}
+          value={curMsaVal}
+          onChange={(e) => setCurMsaVal(e.target.value)}
           autoComplete="off"
         />
         <button type="submit" onClick={onSubmit}>
