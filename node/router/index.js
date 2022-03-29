18742d1403d0027e8ecc0b824ec6a76d48543cfb
宋德康 <1494403731@qq.com>
2022-03-29 23:57:50 +0800

    :alembic: :memo:

--- node/router/index.js
+++ node/router/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import axios from 'axios'
 import { UserModel } from '../models/index.js'
 const router = express.Router() // 得到路由器对象
 /**
@@ -26,6 +27,10 @@ router.post('/login', (req, res) => {
             data: user,
         })
     })
+    // 试下后端获取 乐享 token
+    // 下面这原来试过下 获取乐享token 不过算是敏感信息 删掉的好
+    // 果然是后端没有跨域限制（废话一句）
+    // 还有就是axios.post(),不知道怎么不行，还得直接axios()第二个参数里面指定method属性post才可以
 })
 
 export default router
