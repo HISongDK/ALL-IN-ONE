78c369bed939543d38abdcbb0ec16f447a85e709
宋德康 <1494403731@qq.com>
2022-03-30 23:59:59 +0800

    人一能之己十之，人十能之己百之

---
+++ Snippets/30Minutes/CSVToArray.js
@@ -0,0 +1,18 @@
+/**
+ * Converts a comma-separated values(CSV) string to a 2D array.
+ *
+ * Use Array.prototype.indexOf() to find the newline character('\n')
+ * use Array.prototype.slice() to remove the first row(title row) if omitFirstRow is true.
+ * Use String.prototype.split() to create a string for each row.
+ * use String.prototype.split() to separate the values in each row,using the provided delimiter.
+ * omit the second argument, delimiter,to use a default delimiter of ','.
+ * omit the third argument,omitFirstRow,to include the first row(title row) of the CSV string.
+ */
+
+const CSVToArray = (data, delimiter = ',', omitFirstRow = false) => {
+    data.slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+        .split('\n')
+        .map((v) => v.split(delimiter))
+}
+
+CSVToArray('a,b\nc,d')
---
+++ Snippets/30Minutes/README.md
@@ -0,0 +1,9 @@
+# 30 Minutes Code Snippets
+
+跟着人家说 30 seconds 属实是太看的起我自己了
+
+我看是得 30 Minutes
+
+技术不行，心态摆正啊
+
+“人一能之己十之，人十能之己百之”
