f8cb56fdcdf7d421474cd47f49362c38ccbd62dc
宋德康 <1494403731@qq.com>
2022-04-16 23:43:21 +0800

    :beers: useTitle

---
+++ Snippets/30s-react/useTitle.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable */
+const useTitle = (title) => {
+    const documentDefined = typeof document !== 'undefined'
+    const originalTitle = React.useRef(documentDefined ? document.title : null)
+
+    React.useEffect(() => {
+        if (!documentDefined) return
+        if (document.title !== title) document.title = title
+        return () => (document.title = originalTitle.current)
+    }, [])
+}
+
+const Alert = () => {
+    useTitle('Alert')
+    return <p>Alert! Title has changed</p>
+}
+
+const App = () => {
+    const [alertOpen, setAlertOpen] = React.useState(false)
+
+    return (
+        <div>
+            <button onClick={() => setAlertOpen(!alertOpen)}>
+                Toggle alert
+            </button>
+            {alertOpen && <Alert />}
+        </div>
+    )
+}
+
+ReactDOM.render(<App></App>, document.getElementById('root'))
