f4821e1e27a2d0b00897dd3c2acf7de49bcc3ab9
宋德康 <1494403731@qq.com>
2022-05-07 02:20:51 +0800

    craco 配置 webpack 插件

--- crte/craco.config.js
+++ crte/craco.config.js
@@ -1,22 +1,117 @@
-const CracoLessPlugin = require('craco-less')
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
+// const CracoAntDesignPlugin = require('craco-antd')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const CracoVtkPlugin = require('craco-vtk')
+const WebpackBar = require('webpackbar')
+const FastRefreshCracoPlugin = require('craco-fast-refresh')
+
+// const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 const pathResolve = (pathUrl) => path.join(__dirname, pathUrl)
 
+const { whenDev, whenProd } = require('@craco/craco')
+
 module.exports = {
   webpack: {
+    // 别名配置
     alias: {
-      '@': pathResolve('./src'),
+      '@': pathResolve('src'),
       '@page': pathResolve('./src/page'),
       '@image': pathResolve('./src/static/image'),
     },
+    plugins: [
+      // webpack构建进度条
+      new WebpackBar({
+        profile: true,
+      }),
+
+      // new CircularDependencyPlugin({
+      //   exclude: /node_modules/,
+      //   include: /src/,
+      //   failOnError: true,
+      //   allowAsyncCycles: false,
+      //   cwd: process.cwd(),
+      // }),
+
+      // 查看打包的进度
+      // new SimpleProgressWebpackPlugin(),
+      // 查看打包分析
+      new BundleAnalyzerPlugin(),
+      // 替换moment为dayjs
+      // new AntdDayjsWebpackPlugin(),
+      ...whenProd(
+        () => [
+          new UglifyJsPlugin({
+            uglifyOptions: {
+              // 删除注释
+              output: {
+                comments: false,
+              },
+              compress: {
+                drop_console: true, // 删除所有调式带有console的
+                drop_debugger: true,
+                pure_funcs: ['console.log'], // 删除console.log
+              },
+            },
+          }),
+          // 打压缩包
+          // new CompressionWebpackPlugin({
+          //   algorithm: 'gzip',
+          //   test: /\.js$|\.html$|\.css$/, // 匹配文件名
+          //   threshold: 1024,
+          //   minRatio: 0.8,
+          // }),
+        ],
+        [],
+      ),
+    ],
+    configure: (webpackConfig, { env, paths }) => {
+      paths.appBuild = 'dist'
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        path: path.resolve(__dirname, 'dist'),
+        // publicPath: "/",
+      }
+      return webpackConfig
+    },
   },
+
   plugins: [
+    // 热更新
+    ...whenDev(
+      () => [
+        {
+          plugin: FastRefreshCracoPlugin,
+        },
+        {
+          plugin: CracoVtkPlugin(),
+        },
+        // {
+        // plugin: new AntdDayjsWebpackPlugin(),
+        // },
+      ],
+      [],
+    ),
+
+    // { plugin: CracoAntDesignPlugin },
     {
-      // 配置 craco-less 插件，以支持 less 使用
       plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            // modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
     },
   ],
+
   devServer: {
     // 跨域代理配置
     // proxy: {
