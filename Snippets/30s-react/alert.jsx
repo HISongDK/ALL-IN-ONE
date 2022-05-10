f0de46b4723e18d7c0a9ef8787976af9afce32b1
宋德康 <1494403731@qq.com>
2022-05-10 23:55:54 +0800

    :beers: 这啥玩意啊

---
+++ Snippets/30s-react/alert.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+const Alert = ({ isDefaultShown = false, timeout = 250, type, message }) => {
+    const [isShown, setIsShown] = React.useState(isDefaultShown)
+    const [isLeaving, setIsLeaving] = React.useState(false)
+
+    let timeoutId = null
+
+    React.useEffect(() => {
+        setIsShown(true)
+        return () => {
+            clearTimeout(timeoutId)
+        }
+    }, [isDefaultShown, timeout, timeoutId])
+
+    const closeAlert = () => {
+        setIsLeaving(true)
+        timeoutId = setTimeout(() => {
+            setIsLeaving(false)
+            setIsShown(false)
+        }, timeout)
+    }
+
+    return (
+        isShown && (
+            <div
+                className={`alert ${type} ${isLeaving ? 'leaving' : ''}`}
+                role="alert"
+            >
+                <button className="close" onClick={closeAlert}>
+                    {message}
+                </button>
+            </div>
+        )
+    )
+}
+
+ReactDOM.render(
+    <Alert type="warning" message="This is info" />,
+    document.getElementById('root'),
+)
