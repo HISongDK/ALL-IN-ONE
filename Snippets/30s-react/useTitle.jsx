dec0562cc9170be060551911d91633f2bbd115c4
宋德康 <1494403731@qq.com>
2022-05-14 23:43:42 +0800

    :beers: useTitle

--- Snippets/30s-react/useTitle.jsx
+++ Snippets/30s-react/useTitle.jsx
@@ -1,11 +1,3 @@
-/**
- * Sets the title of the page
- *
- * Use typeof to determine if the Document is defined or not
- * Use the useRef hook to store the original title of the Document,if defined
- * Use the useEffect hook to set Document.title to the passed value when the component mounts and clean up when unmounting
- */
-/* eslint-disable */
 const useTitle = (title) => {
     const documentDefined = typeof document !== 'undefined'
     const originalTitle = React.useRef(documentDefined ? document.title : null)
@@ -13,8 +5,10 @@ const useTitle = (title) => {
     React.useEffect(() => {
         if (!documentDefined) return
         if (document.title !== title) document.title = title
-        return () => (document.title = originalTitle.current)
-    }, [])
+        return () => {
+            document.title = originalTitle.current
+        }
+    })
 }
 
 const Alert = () => {
@@ -28,11 +22,11 @@ const App = () => {
     return (
         <div>
             <button onClick={() => setAlertOpen(!alertOpen)}>
-                Toggle alert
+                Toggle Alert
             </button>
             {alertOpen && <Alert />}
         </div>
     )
 }
 
-ReactDOM.render(<App></App>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
