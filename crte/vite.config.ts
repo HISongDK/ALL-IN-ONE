45f15c2e947beda316f600499f289ff2538b3473
宋德康 <1494403731@qq.com>
2022-10-15 18:04:26 +0800

    Vite 配置

--- crte/vite.config.ts
+++ crte/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig, PluginOption } from 'vite'
+import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
+import usePluginImport from 'vite-plugin-importer'
+import viteCompression from 'vite-plugin-compression'
 
 const pathResolve = (pathUrl: string) => path.join(__dirname, pathUrl)
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       '@': pathResolve('src'),
@@ -16,7 +18,58 @@ export default defineConfig({
       '@constant': pathResolve('src/constant'),
     },
   },
+  plugins: [
+    react(),
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    usePluginImport({
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: 'css',
+    }),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }) as PluginOption,
+    viteCompression({
+      // gzip压缩
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    // chunkSizeWarningLimit: 1500, // 大文件报警阈值设置,不建议使用,
+    rollupOptions: {
+      output: {
+        // 静态资源分类打包
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        manualChunks(id) {
+          // 静态资源分拆打包
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
+      },
+    },
+  },
   server: {
+    // port: 3000,
+    open: true,
     proxy: {
       '/api': 'http://localhost:9000',
     },
